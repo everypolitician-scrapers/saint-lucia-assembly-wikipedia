a2c7baff711eda0f4e0dace1753d3f0b5be7c562
Tony Bowden <tony@tmtm.com>
2017-01-06 10:16:32 +0000

    Rewrite using Scraped

--- scraper.rb
+++ scraper.rb
@@ -2,61 +2,98 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
-require 'nokogiri'
-require 'open-uri'
 require 'pry'
+require 'scraped'
 require 'scraperwiki'
 
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
-def noko_for(url)
-  Nokogiri::HTML(open(url).read)
-end
-
-def party_id_for(party)
-  return 'Saint Lucia Labour Party' if party == 'SLP'
-  return 'United Workers Party' if party == 'UWP'
-  return 'Independent' if party == 'Independent'
-  raise "unknown party; #{party}"
-end
-
-def idify(a)
-  name = a.xpath('./@class').text == 'new' ? a.text : a.attr('title').value
-  name.tr(' ', '-').downcase
-end
-
-def scrape(url)
-  noko = noko_for(url)
-
-  rowdata = lambda do |tr, term|
-    tds = tr.css('td')
-    area = tds[0].text.strip
-
-    data = {
-      id:            idify(tds[1].css('a')),
-      name:          tds[1].text.strip,
-      wikipedia__en: tds[1].xpath('a[not(@class="new")]/@title').text.strip,
-      party:         tds[2].children.first.text.strip,
-      party_id:      '',
-      area:          area,
-      area_id:       'ocd-division/country:lc/constituency:%s' % area.downcase.tr(' ', '-'),
-      term:          term,
-      source:        url,
-    }
-    data[:party_id] = party_id_for(data[:party])
-    data
+class WikipediaPage < Scraped::HTML
+  field :current_composition do
+    current_composition_rows.map do |tr|
+      fragment tr => MemberRow
+    end
   end
 
-  current = noko.xpath('.//h2[contains(.,"Current composition")]/following-sibling::table[1]//tr[td]').map do |row|
-    rowdata.call(row, 9)
+  field :previous_composition do
+    previous_composition_rows.map do |tr|
+      fragment tr => MemberRow
+    end
   end
 
-  previous = noko.xpath('.//h2[contains(.,"Previous composition")]/following-sibling::table[1]//tr[td]').map do |row|
-    rowdata.call(row, 8)
+  private
+
+  def current_composition_rows
+    noko.xpath('.//h2[contains(.,"Current composition")]/following-sibling::table[1]//tr[td]')
   end
 
-  ScraperWiki.save_sqlite(%i(id term), current + previous)
+  def previous_composition_rows
+    noko.xpath('.//h2[contains(.,"Previous composition")]/following-sibling::table[1]//tr[td]')
+  end
 end
 
-scrape('https://en.wikipedia.org/wiki/House_of_Assembly_of_Saint_Lucia')
+class MemberRow < Scraped::HTML
+  field :id do
+    idify(tds[1].css('a'))
+  end
+
+  field :name do
+    tds[1].text.strip
+  end
+
+  field :wikipedia__en do
+    tds[1].xpath('a[not(@class="new")]/@title').text.strip
+  end
+
+  field :party do
+    tds[2].children.first.text.strip
+  end
+
+  field :party_id do
+    party_id_for(party)
+  end
+
+  field :area do
+    area
+  end
+
+  field :area_id do
+    'ocd-division/country:lc/constituency:%s' % area.downcase.tr(' ', '-')
+  end
+
+  field :source do
+    url
+  end
+
+  private
+
+  def tds
+    noko.css('td')
+  end
+
+  def area
+    tds[0].text.strip
+  end
+
+  def party_id_for(party)
+    return 'Saint Lucia Labour Party' if party == 'SLP'
+    return 'United Workers Party' if party == 'UWP'
+    return 'Independent' if party == 'Independent'
+    raise "unknown party; #{party}"
+  end
+
+  def idify(a)
+    name = a.xpath('./@class').text == 'new' ? a.text : a.attr('title').value
+    name.tr(' ', '-').downcase
+  end
+end
+
+url = 'https://en.wikipedia.org/wiki/House_of_Assembly_of_Saint_Lucia'
+page = WikipediaPage.new(response: Scraped::Request.new(url: url).response)
+current = page.current_composition.map { |m| m.to_h.merge(term: 9) }
+previous = page.previous_composition.map { |m| m.to_h.merge(term: 8) }
+
+# puts current + previous
+ScraperWiki.save_sqlite(%i(id term), current + previous)
+
